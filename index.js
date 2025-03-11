37cfca69815fbb007090ac670098f811a72df5ac
pcsuke <110383951+sukepc0824@users.noreply.github.com>
2025-03-11 14:36:28 +0000

    流石に更新

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("data.json")
-        .then(response => response.json())
-        .then(projects => {
-            const container = document.getElementById("portfolio");
-            const projectContainer = document.getElementById("project-detail");
+document.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const response = await fetch("./data/data.json");
+        const projects = await response.json();
 
-            const path = window.location.pathname.replace(/^\//, "");
-            console.log(path)
+        const container = document.getElementById("portfolio");
+        const projectContainer = document.getElementById("project-detail");
 
+        const path = window.location.pathname.replace(/^\//, "");
+
+        projects.forEach(element => {
+            if (element.slug === path && element.type === "app") {
+                renderApp(element)
+            } else {
+                renderHome()
+            }
         })
-        .catch(error => console.error("Error loading projects:", error));
+
+        renderArticle(projects)
+    } catch (error) {
+        console.error("Error loading projects:", error);
+    }
 });
+
+async function renderApp(element){
+    const data = await fetch("./template/app.html")
+    const text = await data.text()
+    let dom = new DOMParser().parseFromString(text, "text/html")
+    dom.querySelector("iframe").src = element.url
+    document.querySelector("body").appendChild(dom.querySelector("main"))
+    const comment = document.createComment(`For Developers / src:"${element.src}"`)
+    document.body.appendChild(comment)
+}
+
+function renderHome() {
+
+}
+
+async function renderArticle(projects){
+    const data = await fetch("./template/article.html")
+    const text = await data.text()
+    let dom = new DOMParser().parseFromString(text, "text/html")
+    projects.forEach(element => {
+        let article = dom.querySelector("article").cloneNode(true)
+        if (element.type === "app"){
+            article.querySelector("a").href = "" //保留
+        }
+        article.querySelector("a").href = element.url
+        article.querySelector("img").src = `./data/img/${element.slug}.png`
+        article.querySelector("h2").innerText = element.name
+        article.querySelector("p").innerText = element.description
+
+        document.querySelector("main").appendChild(article)
+    })
+}
